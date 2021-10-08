c4224452b7350ebe1e917656edd1486333033b8e
u85858558 <Lennisth337@gmail.com>
2021-10-08 12:46:10 +0300

    migration, seeder, graphql

--- database/migrations/2021_10_08_074623_create_wines_table.php
+++ database/migrations/2021_10_08_074623_create_wines_table.php
@@ -14,7 +14,12 @@ class CreateWinesTable extends Migration
     public function up()
     {
         Schema::create('wines', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->text('description');
+            $table->string('color', 10);
+            $table->string('grape_variety', 50);
+            $table->string('country', 50);
             $table->timestamps();
         });
     }
